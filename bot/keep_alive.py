cb5408d4d4112a835f391b9bc3528048c8d8e711
Hrishikesh Dasharath Pawar <88403078+hrishikeshp20@users.noreply.github.com>
2025-04-29 15:36:18 +0530

    Update keep_alive.py

--- bot/keep_alive.py
+++ bot/keep_alive.py
@@ -10,16 +10,16 @@ def home():
 
 @app.route('/uptime-alert/', methods=['POST'])
 def uptime_alert():
-    print("✅ Received ping from Django")
+    print("Received ping from Django")
 
     # Ping Django back
     try:
         django_ping_url = "https://discord-bot-logger-backend.onrender.com/discord-health/"  # Replace with your actual URL
         payload = {"from": "discord_bot", "status": "pong"}
         requests.post(django_ping_url, json=payload, timeout=3)
-        print("↩️ Pinged back Django!")
+        print("↩Pinged back Django!")
     except Exception as e:
-        print("❌ Failed to ping Django:", e)
+        print("Failed to ping Django:", e)
 
     return jsonify({"status": "received"})
 
